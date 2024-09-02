9ccd8b1c1dac9da180947dc70a4023983096a5ce
erickavendanoh <erick-armando167@outlook.es>
2024-09-02 11:13:46 -0600

    Adición de Jetstream, con Livewire, al proyecto

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ Route::group(['middleware' => ['auth']], function(){
     Route::resource('usuarios', UsuarioController::class);
     Route::resource('blogs', BlogController::class);
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
