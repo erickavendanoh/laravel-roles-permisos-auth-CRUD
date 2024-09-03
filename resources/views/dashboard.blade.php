5f3d989eca7890faf3cfba0bc0ede0835f8b95c9
erickavendanoh <erick-armando167@outlook.es>
2024-09-03 09:41:48 -0600

    Reestructura de plantilla principal con respecto a menú sidebar. Adición de notas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,43 +5,10 @@
         </h2>
     </x-slot>
 
-    <div class="row">
-        <div class="col col-md-2">
-            <div class="collapse" id="menuHorizontal">
-                <div class="sidebar-wrapper sidebar-theme">
-                    <nav id="sidebar">
-                        <div class="shadow-bottom"></div>
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                              <a class="nav-link active" aria-current="page" href="#">Active</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                            </li>
-                          </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-        <div class="col col-md-10">
-            <div class="py-12">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                        {{-- <x-welcome /> --}}
-                        content
-                        <h1 class="text-3xl font-bold underline">
-                        Hello world!
-                        </h1>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    {{-- <x-welcome /> --}}
+    content
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+    </h1>
     
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,40 @@
             <!-- Page Content -->
             <main>
                 <div class="container-fluid">
-                    {{ $slot }}
+                    <div class="row">
+                        <div class="col col-md-2">
+                            <div class="collapse" id="menuHorizontal">
+                                <div class="sidebar-wrapper sidebar-theme">
+                                    <nav id="sidebar">
+                                        <div class="shadow-bottom"></div>
+                                        <ul class="nav flex-column">
+                                            <li class="nav-item">
+                                            <a class="nav-link active" aria-current="page" href="#">Active</a>
+                                            </li>
+                                            <li class="nav-item">
+                                            <a class="nav-link" href="#">Link</a>
+                                            </li>
+                                            <li class="nav-item">
+                                            <a class="nav-link" href="#">Link</a>
+                                            </li>
+                                            <li class="nav-item">
+                                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col col-md-10">
+                            <div class="py-12">
+                                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                                        {{ $slot }}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         </div>
